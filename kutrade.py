fa3d14fa7a083dfdaf29339d3a61cdbb4619a1bf
DennisFury <51090576+DennisFury@users.noreply.github.com>
2021-05-12 21:40:08 -0500

    update kutrade.py

--- kutrade.py
+++ kutrade.py
@@ -2,9 +2,10 @@
 #pip install python-kucoin
 
 from kucoin.client import Client
+import time
 
 api_key = ''
-api_secret = ‘’
+api_secret = ''
 api_passphrase = ''
 
 client = Client(api_key, api_secret, api_passphrase)
@@ -13,3 +14,19 @@ amount = int(input('input the amount of USDT to trade with: '))
 to_buy = input('input the symbol to buy: ')
 
 order = client.create_market_order(to_buy + '-USDT', Client.SIDE_BUY, funds=amount)
+order_id = order.get('orderId')
+print('Your Order ID = ' + order_id)
+time.sleep(2)
+
+account = client.get_accounts()
+
+my_coin = next((key for key in account if key['currency'] == to_buy), None)
+no_of_coins = float(my_coin.get('balance'))
+print('Number of ' + to_buy + ' purchased: ' + str(no_of_coins))
+
+while True:
+    ticker = client.get_ticker(to_buy + '-USDT')
+    price = float(ticker.get('price'))
+    my_value = no_of_coins * price
+    print('Value of coins in USDT: ' + str(my_value))
+    time.sleep(1)
